e4087815535e54c2f95d77192f0e9a96a08ea0e6
challa412 <NChalla@trcsolutions.com>
2024-06-21 14:31:11 +0530

    changes in params

--- geoserver_security_latest.js
+++ geoserver_security_latest.js
@@ -8,153 +8,6 @@ const xml2js = require('xml2js');
 
 app.use(express.json());
 
-// Endpoint to fetch WMS layer dynamically
-app.post("/wmservice", async (req, res) => {
-    const { workspace, layerName, bbox, SRS } = req.body;
-  
-    // Validate the presence of required parameters
-    if (!workspace || !layerName || !bbox || !SRS) {
-      return res
-        .status(400)
-        .send("Missing required parameters: workspace, layerName, bbox");
-    }
-  
-    // Ensure bbox parameter is in the correct format, e.g., '-84.81459045410156,39.211692810058594,-83.15721130371094,40.8150749206543'
-    const bboxArray = bbox;
-  
-    // Validate the bbox format
-    if (bboxArray.length !== 4) {
-      return res
-        .status(400)
-        .send(
-          "Invalid BBOX format. It should be in the format: minx,miny,maxx,maxy"
-        );
-    }
-  
-    const [minx, miny, maxx, maxy] = bboxArray;
-  
-    // Define WMS base URL
-    const wmsBaseUrl =
-      "http://geoserver.test-springboard.digital.trccompanies.com/geoserver";
-  
-    // Construct the WMS URL
-    const wmsUrl = `${wmsBaseUrl}/${workspace}/wms`;
-  
-    // Define WMS request parameters
-    const wmsParams = {
-      SERVICE: "WMS",
-      VERSION: "1.1.0",
-      REQUEST: "GetMap",
-      LAYERS: `${workspace}:${layerName}`,
-      STYLES: "",
-      FORMAT: "image/png",
-      TRANSPARENT: true,
-      //   SRS: 'EPSG:3857',
-      SRS: `${SRS}`,
-      BBOX: `${minx},${miny},${maxx},${maxy}`,
-      WIDTH: 768,
-      HEIGHT: 666,
-    };
-  
-    try {
-      // Make POST request to WMS service
-      const response = await axios.post(wmsUrl, null, {
-        params: wmsParams,
-        responseType: "arraybuffer", // Ensure the response is treated as binary data
-      });
-  
-      // Set the appropriate content type for the response
-      res.setHeader("Content-Type", "image/png");
-      // Send the binary data received from WMS server
-      res.send(response.data);
-    } catch (error) {
-      // Handle errors
-      console.error("Error fetching WMS data:", error);
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        res
-          .status(error.response.status)
-          .send(`Error fetching WMS data: ${error.response.statusText}`);
-      } else if (error.request) {
-        // The request was made but no response was received
-        res
-          .status(500)
-          .send("Error fetching WMS data: No response received from server");
-      } else {
-        // Something happened in setting up the request that triggered an error
-        res.status(500).send("Error fetching WMS data: Request setup error");
-      }
-    }
-  });
-  
-  //getting single feature information from WFS service
-  
-  app.get("/features/:workspace/:layername/:featureName/:featureValue", async (req, res) => {
-    // let featureName = req.params.featureName;
-    // let featureValue = req.params.featureValue;
-    const {workspace, layername, featureName, featureValue} = req.params;
-    try {
-      const wfsUrl = `http://geoserver.dev-springboard.digital.trccompanies.com/geoserver/spatialdb/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=spatialdb%3Aleon_county_sections&maxFeatures=50&outputFormat=application%2Fjson&cql_filter=${featureName}=${featureValue}`;
-      const url = `http://geoserver.dev-springboard.digital.trccompanies.com/geoserver/${workspace}/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=${workspace}%3A${layername}&outputFormat=application%2Fjson&cql_filter=${featureName}=${featureValue}`;
-      // Make a request to the WFS URL
-      const response = await axios.get(url);
-  
-      // Send the response received from the WFS server directly to the client
-      res.json(response.data);
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  });
-  
-  // Endpoint to fetch Legend Graphic dynamically
-  
-  app.get("/graphic/:workspace/:layername", async (req, res) => {
-    let workspace = req.params.workspace;
-    let layername = req.params.layername;
-    const legendGraphicUrl = `http://geoserver.dev-springboard.digital.trccompanies.com/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=${workspace}:${layername}`;
-  
-    try {
-      const response = await axios.get(legendGraphicUrl, {
-        responseType: "arraybuffer",
-        validateStatus: function (status) {
-          return status < 500; // Resolve only if the status code is less than 500
-        },
-      });
-  
-      const contentType = response.headers["content-type"];
-  
-      if (contentType.includes("image/png")) {
-        // Set appropriate headers
-        res.set("Content-Type", "image/png");
-  
-        // Send the legend graphic as the response
-        res.send(response.data);
-      } else if (contentType.includes("application/vnd.ogc.se_xml")) {
-        // Convert buffer to string
-        const xmlString = response.data.toString("utf-8");
-  
-        // Parse XML
-        xml2js.parseString(xmlString, (err, result) => {
-          if (err) {
-            console.error("Error parsing XML:", err);
-            res.status(500).send("Error parsing server response");
-          } else {
-            const errorMessage =
-              result?.ServiceExceptionReport?.ServiceException?.[0]?._;
-            console.error("Error fetching legend graphic:", errorMessage);
-            res.status(404).send(errorMessage || "Layer not found");
-          }
-        });
-      } else {
-        res.status(500).send("Unexpected response format");
-      }
-    } catch (error) {
-      console.error("Error fetching legend graphic:", error);
-      res.status(500).send("Error fetching legend graphic");
-    }
-  });
-  
   // Endpoint to fetch capabilities dynamically
   
   app.get("/capabilities/:workspace/:layerName", async (req, res) => {
@@ -200,10 +53,6 @@ app.post("/wmservice", async (req, res) => {
       res.status(500).send("Error fetching capabilities");
     }
   });
-
-
-
- 
 
 
   app.get("/wmservice", async (req, res) => {
@@ -372,9 +221,6 @@ app.post("/wmservice", async (req, res) => {
       res.status(500).json({ error: "Internal Server Error" });
     }
   });
-
-
-
   
 
 app.listen(port , ()=>{
